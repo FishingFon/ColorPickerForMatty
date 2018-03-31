4436c7db12715b3c1cf6f1e411cc2de8c6242544
FishingFon <fishingfon@gmail.com>
2018-04-01 00:39:33 +1000

    Version 0.1 - added http sockets to send http requests and live changes from colorpicker.
    Note: To finish replace url = new URL("YOUR URL") in Main Activity, to the URL required for your device.

--- app/src/main/java/colorpickerformatty/fishingfon/com/colorpickerformatty/MainActivity.java
+++ app/src/main/java/colorpickerformatty/fishingfon/com/colorpickerformatty/MainActivity.java
@@ -29,17 +29,10 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Arrays;
 
+
 public class MainActivity extends AppCompatActivity
         implements ColorPickerFragment.OnFragmentInteractionListener, MultiColorPickerFragment.OnFragmentInteractionListener, SavedColorsFragments.OnFragmentInteractionListener, ColorChangeSelectedListener {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
     private SectionsPagerAdapter mSectionsPagerAdapter;
     private long timeOfLastHttpRequest;
 
